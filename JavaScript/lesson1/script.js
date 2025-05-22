4ff5063e158219c9ca24a4f3cc1dfc9744dbd605
Dmitrii Ozornin <ozornindimitry@uandex.ru>
2025-05-22 17:34:18 +0300

    javascript hw1

---
+++ JavaScript/lesson1/script.js
@@ -0,0 +1,26 @@
+console.log("Текст из script.js");
+
+// задание 2
+// 1
+let num = 123;
+console.log(num);
+
+// 2
+let a = 5;
+a = 7;
+console.log(a);
+
+// 3
+let b = 1 + 2 + 3;
+console.log(b);
+
+// задание 3
+let c = 10;
+let d = 2;
+
+let sum = c + d;
+let deduction = c - d;
+let multiplication = c * d;
+let division = c / d;
+console.log(sum, deduction, multiplication, division);
+console.log(sum + deduction + division + multiplication);
