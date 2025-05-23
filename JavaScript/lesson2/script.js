f294fd43748e4637b74571c0b118b6a58fa24fcd
Dmitrii Ozornin <ozornindimitry@uandex.ru>
2025-05-23 17:47:47 +0300

    семинар 2 джаваскипт

---
+++ JavaScript/lesson2/script.js
@@ -0,0 +1,8 @@
+let answerAge = prompt("Сколько Вам лет");
+if (answerAge !== "") {
+  console.log(`Возраст пользователя ${answerAge}`);
+}
+
+let answerName = prompt("Как Ваше имя?");
+
+console.log(`Добро пожаловать на сайт ${answerName}`);
