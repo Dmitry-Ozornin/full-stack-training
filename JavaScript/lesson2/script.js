9abf195958f0ab01bca001b6d3987b569fbe45da
Dmitrii Ozornin <ozornindimitry@uandex.ru>
2025-05-25 14:56:23 +0300

    dz2

--- JavaScript/lesson2/script.js
+++ JavaScript/lesson2/script.js
@@ -1,8 +1,73 @@
-let answerAge = prompt("Сколько Вам лет");
-if (answerAge !== "") {
-  console.log(`Возраст пользователя ${answerAge}`);
-}
+// let answerAge = prompt("Сколько Вам лет");
+// if (answerAge !== "") {
+//   console.log(`Возраст пользователя ${answerAge}`);
+// }
 
-let answerName = prompt("Как Ваше имя?");
+// let answerName = prompt("Как Ваше имя?");
 
-console.log(`Добро пожаловать на сайт ${answerName}`);
+// console.log(`Добро пожаловать на сайт ${answerName}`);
+
+// let a = "2";
+// let b = "3";
+// console.log(Number(a) + Number(b));
+
+// let c = prompt("введите первое число");
+// let d = prompt("введите второе число");
+// console.log(`Cумма числе равна ${Number(c) + Number(d)}`);
+// console.log(`Разность числе равна ${Number(c) - Number(d)}`);
+// console.log(`Произведение числе равна ${Number(c) * Number(d)}`);
+// console.log(`Частное числе равна ${Number(c) / Number(d)}`);
+// console.log(`Остаток от деления ${Number(c) % Number(d)}`);
+
+// let a = Number(prompt("введите число"));
+
+// if (a > 5) {
+//   console.log("Число больше 5");
+
+// } else if(a < 5) {
+//   console.log("Число меньше 5");
+
+// } else {
+//   console.log("Число равно 5");
+
+// }
+
+// let test1 = Number(prompt("введите первое число"));
+// let test2 = Number(prompt("введите  второе число"));
+
+// if (test1 === test2) {
+//   console.log('равны');
+
+// }else {
+//   console.log("не равны");
+
+// }
+
+// let test1 = Number(prompt("введите первое число"));
+// let test2 = Number(prompt("введите  второе число"));
+
+// if (test1 < test2) {
+//   console.log("Минимальное первое число");
+// } else {
+//   console.log("Минимальное второе число");
+// }
+
+
+ let test1 = Number(prompt("введите первое число"));
+
+ if (test1 > 0 && test1 < 15) {
+  console.log("введенное число больше 0 и меньше 15");
+  
+  
+ } else if (test1 < 0) {
+
+  console.log("Введенное число меньше 0");
+  
+  
+ }else {
+  console.log("Введенное число больше или равно 15");
+  
+
+ }
+  
+ 
